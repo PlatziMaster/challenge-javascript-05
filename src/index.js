f70484382859cd6437810316efaaaed4408abad2
Diego De Granda <degranda@gmail.com>
2020-01-28 12:46:41 -0600

    challenges 05

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  const currentLine = [1];
-
-  const currentLineSize = lineNumber + 1;
-
-  for (let numIndex = 1; numIndex < currentLineSize; numIndex += 1) {
-    // See explanation of this formula in README.
-    currentLine[numIndex] = currentLine[numIndex - 1] * (lineNumber - numIndex + 1) / numIndex;
-  }
-
-  return currentLine;
+  
 }
 
 module.exports = pascalTriangle;
