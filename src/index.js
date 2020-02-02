31c38c35f7052a24db3cf78160f2e99a5d34621b
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-02-02 10:59:52 -0600

    challenge solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,40 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+  let result = [];
+
+  for (let i = 0; i < lineNumber + 1; i++) {
+    result.push(combination(lineNumber, i));
+  }
+  return result;
+}
+
+
+class FactorialWithMemory {
+  constructor() {
+    if (!FactorialWithMemory.instance) {
+      this.memoria = [1];
+      FactorialWithMemory.instance = this;
+    }
+
+    return FactorialWithMemory.instance;
+  }
+
+  cal(num) {
+    if (!this.memoria[num]) {
+      this.memoria[num] = num * this.cal(num - 1);
+    }
+    return this.memoria[num];
+  }
+}
+
+const combination = (n, k) => {
+  let factorial = new FactorialWithMemory();
+  let result;
+  if (n >= k) {
+    result = factorial.cal(n) / (factorial.cal(k) * factorial.cal(n - k));
+  }
+  return result;
 }
 
 module.exports = pascalTriangle;
