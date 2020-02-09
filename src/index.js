50ac3fa3a77fdd091c69da2f2e5b88cf3423669d
Omar Flores Grimontt <omar.flores@3dves.com>
2020-02-08 19:32:43 -0500

    Solved pascal triangle

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@
  * @return {number[]}
  */
 
+const getActualLine = (lastLine) => {
+    const actualLine = []
+    lastLine.forEach((column, index) => {
+        const prevNode = lastLine[index - 1] || 0
+        actualLine.push(prevNode + column)
+    })
+    actualLine.push(lastLine[lastLine.length - 1])
+    return actualLine
+}
+
 const pascalTriangle = (lineNumber) => {
-  
+    const lines = new Map()
+    for (let i = 0; i <= lineNumber; i++) {
+        const lastLine = lines.get(i - 1)
+        if (!lastLine) {
+            lines.set(i, [1])
+        } else {
+            lines.set(i, getActualLine(lastLine))
+        }
+    }
+    return lines.get(lineNumber)
 }
 
 module.exports = pascalTriangle;
