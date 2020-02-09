5185e3d9e2d359e1ac807dbb4fc7a83f7146f60d
dfloresdev <hello@dflores.dev>
2020-02-09 15:32:51 -0600

    mission passed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@
  * @return {number[]}
  */
 
-const pascalTriangle = (lineNumber) => {
-  
-}
+const pascalTriangle = lineNumber => {
+  let number = [1];
+  let numberAux = [];
+  for (let i = 0; i <= lineNumber; i++) {
+    number = [];
+    for (let j = 0; j <= i; j++) {
+      if (numberAux.length >= 2) {
+        if (numberAux[j - 1] && numberAux[j]) {
+          number.push(numberAux[j - 1] + numberAux[j]);
+        } else {
+          number.push(numberAux[0]);
+        }
+      } else {
+        number.push(1);
+      }
+    }
+    numberAux = number;
+  }
+  return number;
+};
 
 module.exports = pascalTriangle;
