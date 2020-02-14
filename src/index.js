55d27df791731de667df0a6337669554a3112b22
MauricioFa <mauriciositel@hotmail.com>
2020-02-14 16:46:41 -0500

    Pascal Triangle: challenge solved"

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+    let oldArray
+    let newArray
+    lineNumber = lineNumber + 1
+    for (i = 1; i <= lineNumber; i++) {
+        newArray = []
+        for (j = 1; j <= i; j++) {
+            if (j === 1 || j === i) {
+                newArray.push(1)
+            } else {
+                newArray.push(oldArray[j - 2] + oldArray[j - 1])
+            }
+        }
+        oldArray = [...newArray]
+    }
+    return newArray
 }
 
 module.exports = pascalTriangle;
