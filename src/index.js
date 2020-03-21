c0af5b3c295c2897a6e786b0e80384e1e39a9e71
Andres Alba <tensinkusum@hotmail.com>
2020-03-21 18:45:52 -0500

    reto 5 javascript Andres Alba

--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@
  * @return {number[]}
  */
 
+ //factorial
+function fact(n){
+	var res=1;
+	if(n===0){
+		res = 1;
+	}
+	else{
+		for(var i = 1; i<=n; i++){
+			res = res*i;
+		}		
+	}
+	return res;
+}
+
+//coeficiente binomial
+function comb(m, k){
+        return (fact(m)/(fact(k)*fact(m-k)));
+}
+
 const pascalTriangle = (lineNumber) => {
-  
+    var arr =[];
+    for(var p = 0; p<=lineNumber; p++){
+		for(var q = 0; q<=p; q++){
+			arr[q]=comb(p,q);
+		}
+    }
+    return arr;
 }
 
 module.exports = pascalTriangle;
