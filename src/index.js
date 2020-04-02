39f2ef07025df00e27328f9d0843a1463bb0767d
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-04-02 08:38:12 -0500

    Propuesta Solución Triangulo de Pascal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+  let number = []
+    if(lineNumber == 0){
+        number.push(1)
+    }else if (lineNumber == 1) {
+        number = [1,1]
+    } else {
+        let casobase = [1,1]
+
+        for (let index = 1; index < lineNumber; index++) {        
+            let aux = [1]
+            for (let i = 0; i < casobase.length-1; i++) {
+                aux.push(casobase[i]+casobase[i+1])        
+            }
+            aux.push(1)
+            casobase = aux
+        }
+        number = casobase
+    }
+    
+  return number
 }
 
 module.exports = pascalTriangle;
