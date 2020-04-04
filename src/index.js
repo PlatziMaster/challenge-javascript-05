f9c362b0b534653e7f1257a037cb5c32e2b2b2f2
Ruben Padilla <rubbenpad2@gmail.com>
2020-04-04 16:44:54 -0500

    challenge-javascript-05 solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  const pascal = [];
+
+  for (let k = 0; k < lineNumber + 1; k++) {
+    if (k === 0 || k === lineNumber) {
+      pascal[k] = 1;
+    } else {
+      pascal[k] = (pascal[k - 1] * (lineNumber + 1 - k)) / k;
+    }
+  }
+
+  return pascal;
+};
 
 module.exports = pascalTriangle;
