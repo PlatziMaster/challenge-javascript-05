a000b1687514572ee2b0d291eb430fdab8c29e61
Jared Ortega <iamyoujared@MacBook-Air-de-Jared.local>
2020-04-04 22:27:27 -0600

    Reto solucionado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+   let pascal = [];
+
+  for (let i = 0; i <= lineNumber; i++) {
+    pascal[i] = new Array(i + 1).fill(1);
+  }
+
+  for (let i = 2; i <= lineNumber; i++) {
+    for (let j = 1; j < pascal[i].length - 1; j++) {
+      pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+    }
+  }
+
+  return pascal[lineNumber];
 }
 
 module.exports = pascalTriangle;
