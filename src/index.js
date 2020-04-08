3734f7734767b7b5d3a6b8762221c93adb5d41f6
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-04-07 22:26:49 -0500

    reto solucionado, triangulo de pascal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+
+  if (lineNumber == 0) {
+      let fila = [1];
+      return fila;
+  }else{
+      let fila = [0, 1, 0];
+      let nuevaFila = [];
+      for(let n = 1; n <= lineNumber; n++){
+        let i = 1
+        for (i; i < fila.length; i++) {
+          let num1 = fila[i - 1] + fila[i];
+          nuevaFila.push(num1);
+        }
+        fila = [0, ...nuevaFila, 0];
+        nuevaFila = [];
+      }
+      return fila.slice(1, fila.length-1);
+  }
 }
 
 module.exports = pascalTriangle;
