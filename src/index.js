87dcd88630d4959c7aecb453bd900eaf8175967c
Iván Chipatecua <ivchip@gmail.com>
2020-04-25 18:10:51 -0500

    Challenge javascript 05 completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-/**
- * @param {number} lineNumber - zero based.
- * @return {number[]}
- */
-
 const pascalTriangle = (lineNumber) => {
-  
+  function pascalRecursive(n, array) {
+    if (n < 1) return array;
+    const prevTier = array[array.length - 1];
+    const curTier = [1];
+    for (let i = 1; i < prevTier.length; i++) {
+      curTier[i] = prevTier[i] + prevTier[i - 1];
+    }
+    curTier.push(1);
+    array.push(curTier);
+    return pascalRecursive(n - 1, array);
+  }
+
+  const triangle = pascalRecursive(lineNumber, [[1]]);
+  return triangle[lineNumber];
 }
 
 module.exports = pascalTriangle;
