856c3fa2b2ac54cec67e060b4b9b865676d74246
camilo <velandiacamilo356@gmail.com>
2020-04-25 20:52:12 -0500

    solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  let result = new Array(lineNumber + 1);
+  // console.log(result);
+  for (let i = 0; i <= lineNumber; i++) {
+    result[i] = new Array(i + 1).fill(1);
+    //console.log(result);
+  }
+  for (let i = 2; i <= lineNumber; i++)
+    for (let j = 1; j < result[i].length - 1; j++)
+      result[i][j] =
+        result[i - 1][j - 1] + result[i - 1][j];
+  //console.log(result);
+  return result[lineNumber];
+};
+//console.log(pascalTriangle(7));
 
 module.exports = pascalTriangle;
