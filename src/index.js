73ec36b9ef52aa63c7290e9cd6615ee18fcaa071
Andres Campuzano <campuxano88@gmail.com>
2020-05-09 02:33:25 -0500

    challenge done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@
  * @return {number[]}
  */
 
+const generateNextRow = (lastRow) => {
+	let nextRow = new Array();
+	nextRow.push(1);
+	for (let i = 0; i < lastRow.length - 1; i++) {
+		nextRow.push(lastRow[i] + lastRow[i + 1]);
+	}
+	nextRow.push(1);
+	return nextRow;
+};
+
 const pascalTriangle = (lineNumber) => {
-  
-}
+	if (lineNumber === 0) {
+		return [1];
+	}
+
+	return generateNextRow(pascalTriangle(lineNumber - 1));
+};
 
 module.exports = pascalTriangle;
