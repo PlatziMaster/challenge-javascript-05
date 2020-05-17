d3f797a75f78c284a650c26dd11265f266c076fa
Jedp <jedp082@gmail.com>
2020-05-17 18:33:20 -0500

    Challenge Done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,47 @@
  * @return {number[]}
  */
 
-const pascalTriangle = (lineNumber) => {
-  
+const crtNewRow = (arr, n) => {
+    let num1 = 0;
+    let num2 = 0;
+    let nxtLine = [];
+
+    for(let i = 0; i < n-1; i++){
+        num1 = arr[i]
+        num2 = arr[i+1]
+        nxtLine.push(num1 + num2)
+      }
+
+      nxtLine.push(1)
+      return nxtLine;
 }
+
+const pascalTriangle = (lineNumber) => {
+  let newLine = [];
+  let befLine = [];
+
+  for(let i = 0; i <= lineNumber; i++){
+    if(i < 2){
+      befLine.push(i);      
+      newLine = crtNewRow(befLine, befLine.length);
+    }else {
+      befLine = newLine;
+      befLine.unshift(0);
+      newLine = crtNewRow(befLine, befLine.length);
+    }
+  }
+
+  console.log(newLine)
+  return newLine;
+}
+
+pascalTriangle(0)
+pascalTriangle(1)
+pascalTriangle(2)
+pascalTriangle(3)
+pascalTriangle(4)
+pascalTriangle(5)
+pascalTriangle(6)
+pascalTriangle(7)
 
 module.exports = pascalTriangle;
