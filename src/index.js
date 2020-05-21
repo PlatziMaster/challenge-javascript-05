c3f84fdca6c592e71d5305ce8107657792ad9dd5
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-05-20 23:53:22 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  let pascalArray = [];
+  for (let i = 0; i <= lineNumber; i++) {
+    let iteration = 1;
+    for (let j = 0; j <= i; j++) {
+      i === lineNumber ? pascalArray.push(iteration) : "";
+      iteration = (iteration * (i - j)) / (j + 1);
+    }
+  }
+  return pascalArray;
+};
 
 module.exports = pascalTriangle;
