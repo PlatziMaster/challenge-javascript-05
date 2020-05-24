f01969767481b0b7e92d56e918372e776d14ac48
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-05-23 20:27:06 -0500

    Challenger resuelto con factorial y combinaciones

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@
  * @return {number[]}
  */
 
+function factorial(n){
+    let resultado = 1;
+    for(let i = 1; i <= n; i++)
+        resultado *= i;
+
+    return resultado;
+}
+
+function calcFactoria(lineNum, nextLine){
+    return (factorial(lineNum)) / (factorial(nextLine) * factorial(lineNum-nextLine));
+}
+
 const pascalTriangle = (lineNumber) => {
-  
+    let pascal = [];
+    let resul;
+
+    for(let i =0; i <= lineNumber; i++){
+        for(let j = 0; j <= i; j++){
+            let valor = calcFactoria(i, j);
+            pascal.push(valor);
+        }
+        resul = pascal;
+        pascal = [];
+    }
+    return resul;
 }
 
 module.exports = pascalTriangle;
