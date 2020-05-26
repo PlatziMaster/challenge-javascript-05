c09368f92ee760ce74cf81971e3fd28d7d059f6e
afcarrion <af.carrion@uniandes.edu.co>
2020-05-26 17:27:12 -0500

    Solucion del reto y adicion de un caso mas de pruebas

--- src/_test_/index.test.js
+++ src/_test_/index.test.js
@@ -1,7 +1,7 @@
-const pascalTriangle = require('../index');
+const pascalTriangle = require("../index");
 
-describe('pascalTriangle', () => {
-  it('Debe calcular los coeficientes del triángulo de Pascal para un número de línea específico', () => {
+describe("pascalTriangle", () => {
+  it("Debe calcular los coeficientes del triángulo de Pascal para un número de línea específico", () => {
     expect(pascalTriangle(0)).toEqual([1]);
     expect(pascalTriangle(1)).toEqual([1, 1]);
     expect(pascalTriangle(2)).toEqual([1, 2, 1]);
@@ -10,5 +10,6 @@ describe('pascalTriangle', () => {
     expect(pascalTriangle(5)).toEqual([1, 5, 10, 10, 5, 1]);
     expect(pascalTriangle(6)).toEqual([1, 6, 15, 20, 15, 6, 1]);
     expect(pascalTriangle(7)).toEqual([1, 7, 21, 35, 35, 21, 7, 1]);
+    expect(pascalTriangle(8)).toEqual([1, 8, 28, 56, 70, 56, 28, 8, 1]);
   });
 });
--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  let coeficientes = [];
+  for (let i = 0; i <= lineNumber; i++) {
+    coeficientes[i] =
+      calcule(lineNumber) / (calcule(i) * calcule(lineNumber - i));
+  }
+  return coeficientes;
+};
+
+const calcule = (numero) => {
+  if (numero === 0) {
+    return 1;
+  }
+  return numero * calcule(numero - 1);
+};
 
 module.exports = pascalTriangle;
