66c548c82c3117044bddcf81b7b7335338115064
emanuelosva <emanuelosva@gmail.com>
2020-05-27 17:20:58 -0500

    Solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@
  * @return {number[]}
  */
 
+const factorial = (n) => {
+    if (n === 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1)
+    }
+}
+
+const combinatorial = (n, p) => {
+    return factorial(n) / ((factorial(p) * factorial(n - p)))
+}
+
 const pascalTriangle = (lineNumber) => {
-  
+    let serie = []
+    for (let i = 0; i <= lineNumber; i++) {
+        serie.push(combinatorial(lineNumber, i))
+    }
+    return serie
 }
 
 module.exports = pascalTriangle;
