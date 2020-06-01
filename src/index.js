ad1212831afdd71829d0ecf63c749d1a43f3bc04
alejandromunozc <alejandromunozc@gmail.com>
2020-06-01 18:55:59 -0500

    Triangulo de pascal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+    let vector = new Array(lineNumber + 1).fill(1);
+    let vectorAux = new Array(lineNumber + 1).fill(1);
+    for (let j = 1; j < lineNumber; j++) {
+        for (let i = 1; i < j + 1; i++) {
+            vector[i] = vectorAux[i - 1] + vectorAux[i];
+        }
+        for (let k = 0; k < vectorAux.length; k++) {
+            vectorAux[k] = vector[k];
+        }
+    }
+    return vector;
 
+}
 module.exports = pascalTriangle;
