129b04d3ad49f6c800975c8532f62124da8d41b8
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-02 15:51:41 -0500

    pascal triangle solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+const factorial = (n) => {
+  return n === 0 ? 1 : n * factorial(n - 1);
+};
+
 /**
  * @param {number} lineNumber - zero based.
  * @return {number[]}
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  const pascalTriangle = [];
+  for (let i = 0; i <= lineNumber; i++) {
+    pascalTriangle.push(
+      factorial(lineNumber) / (factorial(i) * factorial(lineNumber - i))
+    );
+  }
+  return pascalTriangle;
+};
 
 module.exports = pascalTriangle;
