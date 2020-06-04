c9c506713615aefe5340e011c2267688836c7994
Juan Gaybre <jugaybre@gmail.com>
2020-06-04 16:28:49 -0500

    Reto: Triángulo De Pascal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-/**
- * @param {number} lineNumber - zero based.
- * @return {number[]}
- */
-
-const pascalTriangle = (lineNumber) => {
+const factorial = number => {
+  var res =1
   
+  if(number > 1) {
+    for(let i=number; i>0; i--)
+      res *= i
+  }
+
+  return res
+}
+
+const pascalTriangle = (lineNumber) => {  
+  let rowNumbers = []
+
+  for (let i = 0; i <= lineNumber; i++) {
+    const number = (lineNumber, i) => factorial(lineNumber) / (factorial(i) * factorial(lineNumber - i))
+    const num = number(lineNumber, i)
+    rowNumbers.push(num)
+  }
+
+  return rowNumbers  
 }
 
 module.exports = pascalTriangle;
