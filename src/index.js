863b4d0dc24aae8c1727277c0c459b3bddb77335
Andres Valencia <cryos74@gmail.com>
2020-06-11 14:09:14 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+	let triangle =  []
+
+	for (let i = 0; i <= lineNumber; i++) {
+		if (i === 0) {
+			triangle[i] = [1]
+		} else if (i === 1) {
+			triangle[i] = [1, 1]
+		} else {
+			triangle[i] = [];
+			for (let j = 0; j < triangle[i - 1].length + 1; j++) {
+				if (j === 0 || j === triangle[i -1].length) {
+					triangle[i][j] = 1;
+				} else {
+					triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+				}
+			}
+		}
+	}
+	return triangle[lineNumber]
 }
 
 module.exports = pascalTriangle;
