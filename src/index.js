517921a84e27e3cc8a1205b1f3d145e664738e63
Santiago Forero <ds.foreroa@uniandes.edu.co>
2020-06-15 15:33:29 -0500

    resolve

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+    let resp;
+    (lineNumber > 0) ? resp = [1, 1] : resp = [1];
+
+    for (let i = 0; lineNumber > 1 && i < lineNumber-1; i++) {
+        let temp = [];
+        for (let j = 0; j < resp.length - 1; j++) {
+            let sum = resp[j] + resp[j + 1];
+            temp.push(sum);
+
+        }
+        console.log(temp);
+        if (temp.length > 0) {
+            resp =[1,
+                 ...temp,
+                  1]
+        }
+    }
+
+    return resp;
 }
 
 module.exports = pascalTriangle;
