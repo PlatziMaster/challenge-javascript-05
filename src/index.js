5c2bd806a0a248f87b589721a8e2e4f9c63bd76a
frakxman <frakxman@hotmail.com>
2020-06-14 19:07:06 -0500

    Cahellenge -javascript-05 Ok

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@
  * @return {number[]}
  */
 
-const pascalTriangle = (lineNumber) => {
-  
+const pascalTriangle = ( lineNumber ) => {
+    let init = [];
+    if ( lineNumber == 0 ) {
+        init.push( 1 )
+    } else if ( lineNumber == 1 ) {
+        init = [1, 1]
+    } else {
+        let sum = [1, 1], i = 1;
+        do { 
+            let result = [1], j = 0;
+            while ( j < sum.length-1 ) {
+                result.push ( sum[j] + sum[j + 1] ); 
+                j++;
+            }
+            result.push( 1 );
+            sum = result;
+            i++
+        } while (  i < lineNumber  );
+        init = sum;
+    }
+
+    return init;
 }
 
 module.exports = pascalTriangle;
+
