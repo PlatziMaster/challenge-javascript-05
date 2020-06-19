50a9d40fba88e61a82511f5961a103e195423f44
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-18 23:46:55 -0500

    Reto Terminado

--- src/index.js
+++ src/index.js
@@ -3,8 +3,33 @@
  * @return {number[]}
  */
 
-const pascalTriangle = (lineNumber) => {
+const factorial = (number) => {
+    let res = 1;
+    for (let i = 1; i <= number; i++) {
+      res = res * i;
+    }
+    return res;
+  };
   
+  
+  const cominaciones = (n, k) => {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+  };
+
+const pascalTriangle = (lineNumber) => {
+    let array = [];
+    for (let j = 0; j <= lineNumber; j++) {
+        array.push(cominaciones(lineNumber,j));
+    }
+  return array;
 }
 
 module.exports = pascalTriangle;
+
+
+
+
+
+
+
+
