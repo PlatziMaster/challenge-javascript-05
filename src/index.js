df6bab4713e5a086d421c20ee54650cad4606860
Felipe Merchán <felipe-sc@hotmail.es>
2020-06-19 23:06:36 -0500

    This project has an algorithm for calculate the pascal coefficients.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@
  * @return {number[]}
  */
 
+const factorial = (number) => {
+  if (!this.cache) {
+    this.cache = {}
+  }
+
+  if (this.cache[number]) {
+    return this.cache[number]
+  }
+
+  if (number === 1 || number === 0) {
+    return 1
+  }
+
+  this.cache[number] = number * factorial(number - 1)
+  return this.cache[number]
+}
+
 const pascalTriangle = (lineNumber) => {
-  
+  let coefficients = [];
+  for (let i = 0; i <= lineNumber; i++) {
+    coefficients[i] = factorial(lineNumber) / (factorial(i) * factorial(lineNumber - i));
+  }
+
+  return coefficients;
 }
 
 module.exports = pascalTriangle;
