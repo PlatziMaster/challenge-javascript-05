64a4c1b03af1c22b8019bf1bf647460ea06504ee
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-20 19:36:42 -0500

    index2.js tiene la solucion

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@
 const pascalTriangle = (lineNumber) => {
   let result;
 
+  const iteraciones = (result, lineNumber) => {
+    let tmp = [];
+    for (let j = 0; j < result.length - 1; j++) {
+      let sum = result[j] + result[j + 1];
+      tmp.push(sum);
+    }
+    result = [1, ...tmp, 1];
+
+    return result.length === lineNumber + 1
+      ? result
+      : iteraciones(result, lineNumber);
+  };
+
   if (lineNumber == 0) {
     return [1];
   } else if (lineNumber == 1) {
     return [1, 1];
   } else {
     result = [1, 1];
-
-    const iteraciones = (result, lineNumber) => {
-      let tmp = [];
-      for (let j = 0; j < result.length - 1; j++) {
-        let sum = result[j] + result[j + 1];
-        tmp.push(sum);
-      }
-      result = [1, ...tmp, 1];
-
-      return result.length === lineNumber + 1
-        ? result
-        : iteraciones(result, lineNumber);
-    };
     iteraciones(result, lineNumber);
   }
 };
