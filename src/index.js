e9eb90231f2ab1213e529bf9645b954d3d254ac0
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-06-23 10:50:28 -0500

    solved. By CamiloSuarez

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@
  * @return {number[]}
  */
 
-const pascalTriangle = (lineNumber) => {
-  
-}
+const generateNextRow= (lastRow)=>{
+    let nextRow = []
+    
+    nextRow.push(1)
 
+    for(let i=0; i<lastRow.length -1; i++){
+      nextRow.push(lastRow[i]+ lastRow[i+1])
+    }
+    nextRow.push(1)
+    return nextRow
+  }
+  
+  const pascalTriangle = (lineNumber) => {
+  if(lineNumber === 0){
+    return [1]
+  }
+  return generateNextRow(pascalTriangle(lineNumber-1))
+}
 module.exports = pascalTriangle;
