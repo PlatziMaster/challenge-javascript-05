234bbb5be826a3ba3f2786be376ce35e67cdbe76
Juan Espitia <juanespitia92@hotmail.com>
2020-07-05 17:21:36 -0500

    Se realiza una funcion factorial, para dejar dentro de la funcion de triagulos de pascal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+        var dato = lineNumber+1;
+        var arreglito = new Array(dato);
+        function factorial(a){
+            let b = 1;
+            for(var j = a; j>0 ; j--){
+                b *= j;
+            }
+            return b;
+        }
+        for(var i=0; i<dato; i++){
+            arreglito[i]=(factorial(lineNumber)/(factorial(i)*factorial(lineNumber-i)));
+        }
+        return arreglito;
+
 }
 
 module.exports = pascalTriangle;
