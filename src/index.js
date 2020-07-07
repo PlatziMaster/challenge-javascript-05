9c7a8285e4a93185e6a1d5081bd6e3c89d6dae2e
Andres Moreno <andresmorenojf@gmail.com>
2020-07-07 13:18:33 -0500

    Challenge 5 resolved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+  let arrayPreve = [];
+  let arrayPascal = [];
+  for (i = 0; i <= lineNumber; i++) {
+    arrayPascal.push(0)
+  }
+  arrayPascal[0] = 1
+  for (i = 1; i <= lineNumber; i++) {
+    for (j = 1; j <= i; j++) {
+      arrayPreve[j] = arrayPascal[j - 1] + arrayPascal[j];
+    }
+    for (j = 1; j <= i; j++) {
+      arrayPascal[j] = arrayPreve[j]
+    }
+  }
+  return arrayPascal
 }
+
+
 
 module.exports = pascalTriangle;
