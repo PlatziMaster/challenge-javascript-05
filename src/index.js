16964567f25891804d9ba3ae50eb28ba618dca2a
Dimas Mendoza <damendoza98@gmail.com>
2020-07-12 20:40:08 -0500

    Se cumple el reto 5 espero seguir encontrando mas soluciones

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@
  */
 
 const pascalTriangle = (lineNumber) => {
+    let trianguloPascal = [1];
+    for (let cont1 = 0; cont1 <= lineNumber; cont1++) {
+        let temporal = Array();
+        temporal[0] = 1;
+        temporal[cont1] = 1;
+        for (let cont2 = 1; cont2 < cont1; cont2++) {
+            if (cont2 != cont1) {
+                temporal[cont2] = trianguloPascal[cont2] + trianguloPascal[cont2-1];
+            }
+        }
+        trianguloPascal = temporal;
+    }
+    return trianguloPascal;
   
 }
 
