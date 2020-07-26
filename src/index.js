095ff9fdd2c17afb66d7ec529f4ff8da23437d50
Paul Montoya <52388061+PaulMoro@users.noreply.github.com>
2020-07-26 07:25:13 -0500

    Challenge solve (I'm dancing)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+
+  let arrayTirangule = [];
+
+  if (lineNumber == 0) {
+    return arrayTirangule = [1];
+  } if (lineNumber == 1) {
+    return arrayTirangule = [1,1];
+  } else {
+    for (i = 0; i <= lineNumber; i++) {
+      let arriba = factorial(lineNumber);
+      let a = factorial(i);
+      let b = factorial(lineNumber - i);
+      let c = a * b;
+      arrayTirangule[i] = arriba / c;
+    }
+    return arrayTirangule;
+  }
 }
+
+  const factorial = (n) => {
+    if (n === 0){ 
+      return 1; 
+    } return n * factorial (n-1); 
+  };
+
+ 
 
 module.exports = pascalTriangle;
