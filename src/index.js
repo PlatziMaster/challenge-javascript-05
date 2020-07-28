058e165604eeac4fee40d19d30db8c9ca3965071
Gerardo Garavito <garavitogerardo@gmail.com>
2020-07-28 17:03:48 -0500

    reto completado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
+	let defaultTriangle = [1]
+
+	let pascalTriangle = [1]
+	let position = 1 //irá i++
+	let exponent = lineNumber //irá i--
+	let coeficient = 1
+
+	if (lineNumber === 0){
+		return defaultTriangle
+	} else if (lineNumber === 1){
+		defaultTriangle.push(1)
+		return defaultTriangle
+	}
+
+	for (position=1; position < lineNumber+1; position++){
+		coeficient = exponent * coeficient / position
+		pascalTriangle.push(coeficient)
+
+		exponent--
+	}
+
+	return pascalTriangle
 }
 
 module.exports = pascalTriangle;
