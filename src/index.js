ba6500c4d8ef2e318e0d4cf05ea366bc8b7ce8c1
Sergio Ruiz <shecho27@gmail.com>
2020-08-03 20:41:43 -0500

    Edit th function: Pascal Triangle

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  const arrayA = [[1]];
+  for (let j = 0; j < lineNumber; j++) {
+    const arrayB = [1];
+    for (let i = 1; i < arrayA[j].length; i++) {
+      arrayB.push(arrayA[j][i - 1] + arrayA[j][i]);
+    }
+    arrayB.push(1);
+    arrayA.push(arrayB);
+    console.log(arrayA); // Impresión de prueba
+  }
+  return arrayA[lineNumber];
+};
 
 module.exports = pascalTriangle;
