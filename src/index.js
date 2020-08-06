76bb572e7ea36cbf60c625f214862b6eeb1ae3f7
chato1337 <chato1337@gmail.com>
2020-08-06 09:16:56 -0500

    solucion al reto por didier cuetia

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+    let iterador = lineNumber+1;
+
+    let triangulo = [];
+
+	triangulo.push([1]);
+
+	for (let i = 1; i < iterador; i++) {
+		let filaAnterior = triangulo[i - 1];
+		let nuevaFila = [];
+
+		nuevaFila.push(1);
+
+		for (let j = 1; j < filaAnterior.length; j++) {
+			nuevaFila.push(filaAnterior[j - 1] + filaAnterior[j]);
+		}
+
+		nuevaFila.push(1);
+		triangulo.push(nuevaFila);
+	}
+
+    console.log(triangulo[lineNumber]);
+	return triangulo[lineNumber];
+};
 
 module.exports = pascalTriangle;
