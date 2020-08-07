14f3f513762161adc101dd43f821408aa4e55662
johan-avila <johan-avila@outlook.com>
2020-08-06 19:12:44 -0500

    Terminado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+    let array = new Array(lineNumber + 1)
+    for (i = 0; i < array.length; i++) {
+        array[i] = new Array(i + 1)
 
+        for (j = 0; j <= array[i].length; j++) {
+            array[i][0] = 1
+
+            if (array[i - 1] && array[i - 1][j - 1]) {
+                array[i][j] = array[i - 1][j] + array[i - 1][j - 1]
+            }
+
+
+            array[i][array[i].length - 1] = 1
+        }
+    }
+    return array[lineNumber]
+
+}
+console.log(pascalTriangle(7))
 module.exports = pascalTriangle;
