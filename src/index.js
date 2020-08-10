b31e388505b1d75555bee81e42d37126d16fda47
JoelNieto <joenieto90@hotmail.com>
2020-08-10 16:08:57 -0500

    Solved Challenge JavaScript 05

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+    let TempArray = [1, 1];
+    let ActualArray = [1];
+
+    if(lineNumber === 0) {
+        return ActualArray;
+    } 
+
+    if(lineNumber === 1) {
+        return TempArray;
+    }
+
+    for (let i = 2; i <= lineNumber; i++)
+    {
+        ActualArray = [1];
+        for(let j = 1; j < i; j++)
+        {
+            ActualArray.push(TempArray[j-1] + TempArray[j]);
+        }
+        ActualArray.push(1);
+        TempArray = ActualArray;
+    }
+    return ActualArray;
+};
 
 module.exports = pascalTriangle;
