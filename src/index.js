9f340544fd121538833c0d824d34b6ae9a124106
Efrain Hernandez <efrainhg2050@gmail.com>
2020-10-01 17:05:16 -0500

    Solución al reto 5 de JavaScript

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+  let arr = [];
+  let arrSecond = [1, 1];
+
+  if (lineNumber === 0) {
+      arr.push(1);
+      return arr;
+  }
+  if (lineNumber === 1) {
+    arr.push(1, 1);
+    return arr;
+  }
+  if (lineNumber > 1) { 
+      for (let i = 2; i<= lineNumber; i++) {
+          arr = [1];
+          for(let j = 1; j < i; j++) {
+              arr.push(arrSecond[j-1] + arrSecond[j]);
+          }
+          arr.push(1);
+          arrSecond = arr;
+      }
+      return arr;
+  }
+};
 
 module.exports = pascalTriangle;
