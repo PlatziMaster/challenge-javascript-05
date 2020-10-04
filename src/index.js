6884da0458f14b1a499d9fa77a08ad67537ead59
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-03 22:50:01 -0500

    Adding the pascal triangle function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@
  * @param {number} lineNumber - zero based.
  * @return {number[]}
  */
+function factorial (number) {
+  if (number === 0) {
+    return 1
+  }
+  return  factorial(number - 1) * number
+}
 
+function algorithm (a) {
+  return function () {
+    let elements = []
+    for (let i = 0; i <= a; i++) {
+      elements = elements.concat(factorial(a) / Math.imul(factorial(a - i), factorial(i)))
+    }
+    return elements
+  }
+}
 const pascalTriangle = (lineNumber) => {
-  
+  const result = algorithm(lineNumber)
+  return result()
 }
 
 module.exports = pascalTriangle;
